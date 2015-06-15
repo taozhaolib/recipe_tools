e2f61984d89cb88e175fe9d94548b4550b69341d
Logan Cox <lmc@ou.edu>
2015-06-15 18:48:49 -0500

    cleaning up error handling

--- harvest_marc.php
+++ harvest_marc.php
@@ -4,17 +4,15 @@ require 'vendor/autoload.php';
 use GuzzleHttp\Client;
 
 
-
-if(! $itemfile = $argv[1] ) {
-    exit("no item csv file specified");
+if(! $itemfile =@ $argv[1] ) {
+    exit("no item csv file specified\n");
 }
-
 if(! $csvfh = @fopen( "items.csv", "r" ) ) {
-    exit("Couldn't open file");
+    exit("Couldn't open file\n");
 }
 
 
-# setup Guzzle HTTP client to make requrests for marcxml files
+# set up Guzzle client to make requests for marcxml 
 $client = new Client(['base_uri' => 'http://52.0.88.11']); 
 
 
@@ -27,13 +25,11 @@ while($line = fgetcsv($csvfh ) ){
 	continue;
     }
 
-
     # get marcxml for a book
     $mssid= $line[1];
     $bagname = $line[2];
-    fwrite(STDOUT, "processing mssid:".$mssid. " for bag ".$bagname."\n" );
+    fwrite(STDOUT, "processing mssid ".$mssid. " for bag ".$bagname."\n" );
     $response =$client->get('.', ['query' => ['bib_id' => $mssid]]);
-
 
     # save it to a file based on the bag name
     if( $outfh = @fopen( $bagname.".xml", "w" ) ) {
